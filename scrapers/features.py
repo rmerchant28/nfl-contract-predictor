47d8fa014d9bdc8c3afd83afa369756ac8a06698
rm2bt <rm2bt@virginia.edu>
2026-03-23 14:20:42 -0400

    fixed bug for back up qbs and added weekly data to the repo and fixed front end issues with the new data.

--- scrapers/features.py
+++ scrapers/features.py
@@ -64,7 +64,7 @@ QB_STAT_COLS = [
     "pass_first_downs", "passing_epa",
     "dakota", "pacr",
     "carries", "rushing_yards", "rushing_tds",
-    "games",
+    "games", "attempts_per_game", "is_starter",
 ]
 
 
@@ -98,7 +98,7 @@ WR_STAT_COLS = [
     "rec_tds", "catch_rate", "rec_first_downs",
     "target_share", "air_yards_share",
     "receiving_epa", "racr", "wopr_x",
-    "games",
+    "games", "targets_per_game",
 ]
 
 RB_STAT_COLS = [
@@ -106,7 +106,7 @@ RB_STAT_COLS = [
     "rush_tds", "rush_first_downs", "fumbles",
     "rushing_epa",
     "targets", "receptions", "rec_yards", "rec_tds",
-    "games",
+    "games", "targets_per_game",
 ]
 
 TE_STAT_COLS = [
@@ -114,7 +114,7 @@ TE_STAT_COLS = [
     "yards_per_reception", "yards_per_target",
     "rec_tds", "catch_rate",
     "receiving_epa", "target_share",
-    "games",
+    "games", "targets_per_game",
 ]
 
 
